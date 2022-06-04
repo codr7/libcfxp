9e217a2657cd27764be27440c30dc939c235af05
Andreas Nilsson <andreas.nilsson@jayway.com>
2022-06-04 23:36:24 +0200

    Formatting.

--- src/cfxp/fix64.c
+++ src/cfxp/fix64.c
@@ -5,14 +5,17 @@ uint64_t cfxp_scale(uint8_t exp) {
   while (exp--) { s *= 10; }
   return s;
 }
-			
+
+
 cfxp_fix64_t cfxp_fix64_new(uint8_t exp, uint64_t val) {
   return (cfxp_fix64_t)((exp & ((1 << CFXP_FIX64_EXP_BITS) - 1)) + (val << CFXP_FIX64_EXP_BITS));
 }
 
+
 uint8_t cfxp_fix64_exp(cfxp_fix64_t x) { return x & ((1 << CFXP_FIX64_EXP_BITS) - 1); }
 
 uint64_t cfxp_fix64_val(cfxp_fix64_t x) { return x >> CFXP_FIX64_EXP_BITS; }
+
 
 cfxp_fix64_t cfxp_fix64_add(cfxp_fix64_t x, cfxp_fix64_t y) {
   uint8_t xe = cfxp_fix64_exp(x);
@@ -31,6 +34,7 @@ cfxp_fix64_t cfxp_fix64_mul(cfxp_fix64_t x, cfxp_fix64_t y) {
 cfxp_fix64_t cfxp_fix64_div(cfxp_fix64_t x, cfxp_fix64_t y) {
   return cfxp_fix64_new(cfxp_fix64_exp(x), cfxp_fix64_val(x) / (cfxp_fix64_val(y) / cfxp_scale(cfxp_fix64_exp(y))));
 }
+
 
 uint64_t cfxp_fix64_trunc(cfxp_fix64_t x) { return cfxp_fix64_val(x) / cfxp_scale(cfxp_fix64_exp(x)); }
 
--- src/cfxp/fix64.h
+++ src/cfxp/fix64.h
@@ -8,12 +8,15 @@
 typedef uint64_t cfxp_fix64_t;
 
 cfxp_fix64_t cfxp_fix64_new(uint8_t exp, uint64_t val);
+
 uint8_t cfxp_fix64_exp(cfxp_fix64_t x);
 uint64_t cfxp_fix64_val(cfxp_fix64_t x);
+
 cfxp_fix64_t cfxp_fix64_add(cfxp_fix64_t x, cfxp_fix64_t y);
 cfxp_fix64_t cfxp_fix64_sub(cfxp_fix64_t x, cfxp_fix64_t y);
 cfxp_fix64_t cfxp_fix64_mul(cfxp_fix64_t x, cfxp_fix64_t y);
 cfxp_fix64_t cfxp_fix64_div(cfxp_fix64_t x, cfxp_fix64_t y);
+
 uint64_t cfxp_fix64_trunc(cfxp_fix64_t x);
 uint64_t cfxp_fix64_frac(cfxp_fix64_t x);
 
